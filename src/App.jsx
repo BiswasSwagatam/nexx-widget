806a12dec94b3816f825e4b0b52967914b31a8eb
BiswasSwagatam <swagatam.04.biswas@gmail.com>
2024-09-23 16:24:01 +0530

    project complete

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button"
+import { Widget } from "./components/Widget"
+
+
+function App() {
+  
+
+  return (
+    <>
+      <Widget projectId='1'/>
+    </>
+  )
+}
+
+export default App
